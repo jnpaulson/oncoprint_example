72cedc6e8597faa7fe1dd4ecd25f74bc75901912
Joseph N. Paulson <jpaulson@jimmy.harvard.edu>
2015-08-26 19:39:09 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,11 +1,23 @@
-shinyUI(pageWithSidebar(
-  headerPanel('oncoprint example'),
-  sidebarPanel(
-    textInput("samples", label = h3("Samples to include:"), value = "1:75"),
-    p(),
-    tags$small("R notation to select column (sample) indices, i.e. c(1:10,60:75)")
-  ),
-  mainPanel(
-    plotOutput('plot1')
-  )
-))
+library(devtools)
+if(!require(oncoprint)){
+  install_github("dakl/oncoprint")
+  library(oncoprint)
+}
+
+data(tcga_brca)
+
+vert_x <- theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=.5))
+
+
+shinyServer(function(input, output) {
+
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    tcga_brca[, eval(parse(text=input$samples))]
+  })
+
+  output$plot1 <- renderPlot({
+    oncoprint(selectedData()) + coord_fixed() + vert_x
+  })
+
+})
