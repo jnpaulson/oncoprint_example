4425fcd56948ecf5cd572cf77c96a7de8089c62f
Joseph N. Paulson <jpaulson@jimmy.harvard.edu>
2015-08-26 19:33:30 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,23 +1,20 @@
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+library(devtools)
+if(!require(oncoprint)){
+  install_github("dakl/oncoprint")
+  library(oncoprint)
+}
+data(tcga_brca)
+vert_x <- theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=.5))
 
-shinyServer(function(input, output, session) {
+shinyServer(function(input, output) {
 
   # Combine the selected variables into a new data frame
   selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
-  })
-
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
+    tcga_brca[, eval(parse(text=input$samples))]
   })
 
   output$plot1 <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    oncoprint(selectedData()) + coord_fixed() + vert_x
   })
 
 })
